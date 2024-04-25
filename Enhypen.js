59ca36d7f548d7084e5a7aea7ea5f9ec5ceaef60
Jack161223 <167942356+Jack161223@users.noreply.github.com>
2024-04-24 19:58:01 -0600

    Add files via upload

---
+++ Enhypen.js
@@ -0,0 +1,9 @@
+var p=prompt("Te gustaria saber un poco sobre Enhypen?");
+switch(p){
+    case "si":
+    alert("aqui tienes un poco de informacion");
+    break;
+    case "no":
+     alert("es lamentable tu desicion, pero aqui tienes informacion");
+     break;
+}
